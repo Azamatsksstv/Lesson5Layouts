3497037ba59a4688ffcce0493e2147c02063c593
Azamat Zhambylbayev <200103392@stu.sdu.edu.kz>
2022-10-02 03:38:43 +0500

    Calculate the tip - 4. Calculate the tip - Get the tip percentage(1)

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -18,5 +18,7 @@ class MainActivity : AppCompatActivity() {
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
+
+        val selectedId = binding.tipOptions.checkedRadioButtonId
     }
 }
