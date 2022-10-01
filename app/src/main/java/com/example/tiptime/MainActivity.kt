4282fc1fc305b9cb02da61efea4a41ac034778a6
Azamat Zhambylbayev <200103392@stu.sdu.edu.kz>
2022-10-02 03:44:16 +0500

    Calculate the tip - 4. Calculate the tip - Format the tip(5)

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
